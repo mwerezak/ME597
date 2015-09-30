ac4a9759fdc53786a16b1143d56e2bc012435113
HarpyEagle <mwerezak@gmail.com>
2015-09-29 20:52:30 -0400

    Minor example node cleanup

--- Lab1/src/turtlebot_example/src/turtlebot_example_node.cpp
+++ Lab1/src/turtlebot_example/src/turtlebot_example_node.cpp
@@ -12,41 +12,51 @@
 #include <ros/ros.h>
 #include <geometry_msgs/PoseWithCovarianceStamped.h>
 #include <geometry_msgs/Twist.h>
+#include <geometry_msgs/Pose2D.h>
 #include <tf/transform_datatypes.h>
 
+using namespace ros;
+using namespace geometry_msgs;
+
+// Convenience macros
+#define POSECOV_X(pose_with_covariance) (pose_with_covariance.pose.position.x)
+#define POSECOV_Y(pose_with_covariance) (pose_with_covariance.pose.position.y)
+#define POSECOV_Z(pose_with_covariance) (pose_with_covariance.pose.position.z)
+#define POSECOV_O(pose_with_covariance) (pose_with_covariance.pose.orientation)
+
 //Callback function for the Position topic 
-void pose_callback(const geometry_msgs::PoseWithCovarianceStamped& msg)
+void pose_callback(const PoseWithCovarianceStamped& msg)
 {
 	//This function is called when a new pose message is received
 
-	double X = msg.pose.pose.position.x; // Robot X psotition
-	double Y = msg.pose.pose.position.y; // Robot Y psotition
-	double Yaw = tf::getYaw(msg.pose.pose.orientation); // Robot Yaw
+	double X = POSECOV_X(msg.pose); // Robot X psotition
+	double Y = POSECOV_Y(msg.pose); // Robot Y psotition
+	double Yaw = tf::getYaw(POSECOV_O(msg.pose)); // Robot Yaw
 	ROS_DEBUG("pose_callback X: %f Y: %f Yaw: %f", X, Y, Yaw);
 }
 
 int main(int argc, char **argv)
 {
 	//Initialize the ROS framework
-	ros::init(argc,argv,"main_control");
-	ros::NodeHandle node_handle;
+	init(argc,argv,"main_control");
+	NodeHandle node_handle;
 
 	//Subscribe to the desired topics and assign callbacks
-	ros::Subscriber pose_sub = node_handle.subscribe("/amcl_pose", 1, pose_callback);
+	Subscriber pose_sub = node_handle.subscribe("/amcl_pose", 1, pose_callback);
 
 	//Setup topics to Publish from this node
-	ros::Publisher velocity_publisher = node_handle.advertise<geometry_msgs::Twist>("/cmd_vel_mux/input/navi", 1);
+	Publisher velocity_publisher = node_handle.advertise<Twist>("/cmd_vel_mux/input/navi", 1);
     
 	//Velocity control variable
-	geometry_msgs::Twist vel;
+	Twist vel;
 
 	//Set the loop rate
-	ros::Rate loop_rate(20);    //20Hz update rate
+	Rate loop_rate(20);    //20Hz update rate
     
-	while (ros::ok())
+	while (ok())
 	{
 		loop_rate.sleep(); //Maintain the loop rate
-		ros::spinOnce();   //Check for new messages
+		spinOnce();   //Check for new messages
     
 		//Main loop code goes here:
 		vel.linear.x = 0.2; // set linear speed
