0a88e4a3b6ca10b8dedec383fb3d1f936770dc1d
HarpyEagle <mwerezak@gmail.com>
2015-11-05 16:42:41 -0500

    Implements mapping update

--- Lab2/src/lab2_mapping/src/laser_scan.cpp
+++ Lab2/src/lab2_mapping/src/laser_scan.cpp
@@ -1,5 +1,9 @@
 #include "laser_scan.h"
 
+#include <tf/LinearMath/Vector3.h>
+#include <tf/LinearMath/Transform.h>
+#include <tf/LinearMath/Quaternion.h>
+#include <angles/angles.h>
 #include "ray_tracing.h"
 
 static const logit_val OCCUPANCY_HIT_FEATURE = logit(0.9); //occupancy of a cell where the beam hit a feature
@@ -11,10 +15,41 @@ void MappingUpdate(OccupancyGrid& occ_map, const sensor_msgs::LaserScan& scan_da
 	//TODO add transformation to world frame
 	//For now just assume robot is at world origin with default orientation.
 	
+	/*
+	From ROS docs:
+		angles are measured around the positive Z axis (counterclockwise, if Z is up)
+		with zero angle being forward along the x axis.
+	*/
+	const tf::Vector3 scan_basis(1.0, 0.0, 0.0); //unit vector corresponding to zero angle.
+	const tf::Vector3 zaxis(0.0, 0.0, 1.0);
+	
+	tf::Transform pan_transform; //used to rotate the scan with angle
+	
 	//iterate over each beam
-	for(int angle = scan_data.angle_min; angle <= scan_data.angle_max; angle += scan_data.angle_increment)
+	double angle = scan_data.angle_min;
+	int beam_idx = 0;
+	while(angle <= scan_data.angle_max)
 	{
-		//TODO
+		double beam_range = scan_data.ranges[beam_idx];
+		
+		//not sure if ranges[beam_idx] > range_max means nothing was hit or what.
+		//ROS docs say to discard the values so I guess that's what we'll do.
+		if(scan_data.range_min <= beam_range && beam_range <= scan_data.range_max)
+		{
+			tf::Quaternion pan_rotation(zaxis, angle);
+			pan_transform.setRotation(pan_rotation);
+
+			tf::Vector3 beam_start = pan_transform(scan_basis);
+			beam_start *= scan_data.range_min;
+
+			tf::Vector3 beam_end = pan_transform(scan_basis);
+			beam_end *= scan_data.ranges[beam_idx];
+			
+			MapUpdateBeamHit(occ_map, beam_start, beam_end);
+		}
+		
+		angle += scan_data.angle_increment;
+		beam_idx++;
 	}
 }
 
@@ -26,11 +61,11 @@ void _mapUpdateTrace(OccupancyGrid& occ_map, GridRayTrace& trace, logit_val beam
 	{
 		if(trace.hasNext())
 		{
-			occ_map.valueAt(i, j) = beamval;
+			occ_map.valueAt(i, j) += beamval;
 		}
 		else
 		{
-			occ_map.valueAt(i, j) = endval;
+			occ_map.valueAt(i, j) += endval;
 		}
 	}
 }
